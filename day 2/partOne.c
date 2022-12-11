e0570cf7c84f567050d20d6cf2dea25d838acc95
Alexander <c20alm@cs.umu.se>
2022-12-11 15:58:24 +0100

    removed reduntant comment

--- day 2/partOne.c
+++ day 2/partOne.c
@@ -3,19 +3,6 @@
 
 #define BUFF_SIZE 256
 
-/* 
-rock = 1
-paper = 2
-scissors = 3
-
-rock - scissor = -2 win
-scissor - paper = 1 win
-paper - rock = 1 win
-
-rock - paper = -1 loss
-scissor - rock = 2 loss
-paper - scissor = -1 loss */
-
 int main(int argc, char *argv[])
 {
     FILE *input = fopen("input.txt", "r");
